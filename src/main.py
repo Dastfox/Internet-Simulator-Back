1996448c5b35883f101eb88ee64db7e179ed5b06
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-06-29 10:09:31 +0200

    changed from sqlalchemy to sqlmodels

--- src/main.py
+++ src/main.py
@@ -1,54 +1,94 @@
-from fastapi import FastAPI, Depends
-from . import database as dab
-from sqlalchemy.orm import Session
-from . import models as md
-from . import schemas as sc
+from typing import List, Optional
+
+from fastapi import FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class HeroBase(SQLModel):
+    name: str = Field(index=True)
+
+
+class Hero(HeroBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class HeroCreate(HeroBase):
+    pass
+
+
+class HeroRead(HeroBase):
+    id: int
+
+
+class HeroUpdate(SQLModel):
+    name: Optional[str] = None
+
+
+sqlite_file_name = "hero.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 
 app = FastAPI()
 
-md.Base.metadata.create_all(bind=dab.engine)
 
-# keep the connection all the time is ok cuz its a local file
-# Don't on postgre  
-def get_db():
-    try:
-        db = dab.SessionLocal()
-        yield db
-    finally:
-        db.close()
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
-@app.get("/dashboard")
-async def dashboard(db: Session = Depends(get_db)):
-    return db.query(md.Hero).all()
 
-# adding an hero
-@app.post("/heroes")
-async def HeroAdd(hero: sc.Hero, db: Session = Depends(get_db)):
-    hero_model = md.Hero()
-    hero_model.name = hero.name
-    db.add(hero_model)
-    db.commit()
-    
-    return hero
+@app.post("/heroes/", response_model=HeroRead)
+def create_hero(hero: HeroCreate):
+    with Session(engine) as session:
+        db_hero = Hero.from_orm(hero)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+        return db_hero
 
-# Updating a hero  
-@app.put("/details/{id}")
-async def UpdateHero(id: int, hero: sc.Hero, db: Session = Depends(get_db)):
 
-    heroes = db.query(md.Hero).filter(
-        md.Hero.id == id).first()
+@app.get("/heroes/", response_model=List[HeroRead])
+def read_heroes():
+    with Session(engine) as session:
+        return session.exec(select(Hero)).all()
 
-    heroes.name = hero.name
 
-    db.add(heroes)
-    db.commit()
+@app.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return hero
 
-    return hero
-    
-@app.delete("/heroes/")
-async def delete_hero(id: int, db: Session = Depends(get_db)):
-        
-        db.query(md.Hero).filter(md.Hero.id==id).delete()
-        db.commit()
-        return {"deleted": True}
-    
+
+@app.patch("/heroes/{hero_id}", response_model=HeroRead)
+def update_hero(hero_id: int, hero: HeroUpdate):
+    with Session(engine) as session:
+        db_hero = session.get(Hero, hero_id)
+        if not db_hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        hero_data = hero.dict(exclude_unset=True)
+        for key, value in hero_data.items():
+            setattr(db_hero, key, value)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+        return db_hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        session.delete(hero)
+        session.commit()
+        return {"ok": True}
--- src/models.py
+++ src/models.py
@@ -1,12 +1,12 @@
-from enum import unique
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+# from enum import unique
+# from sqlalchemy import Column, Integer, String
+# from sqlalchemy.orm import relationship
 
-from .database import Base
+# from .database import Base
 
-# declaring var in the DB 
-class Hero(Base):
-    __tablename__ = 'hero'
-    # /!\ primary key can only be true on one column (like in django)
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+# # declaring var in the DB 
+# class Hero(Base):
+#     __tablename__ = 'hero'
+#     # /!\ primary key can only be true on one column (like in django)
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
