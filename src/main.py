3d4d6dd62d1a986926f2878e1b767397c0236c7f
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-06-30 09:48:15 +0200

    middleware update

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from typing import List, Optional
-
-from fastapi import FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException, Query
+
 
 
 
@@ -37,19 +37,17 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
+
+
 app = FastAPI()
 
+origins= ['*']
 
-origins = [
-    # "http://localhost/heroes",
-    # "https://localhost.tiangolo.com",
-    # "http://localhost",
-    # "http://localhost:8080",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins='*',
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -60,7 +58,7 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/heroes/", response_model=HeroRead)
+@app.post("/heroes", response_model=HeroRead)
 def create_hero(hero: HeroCreate):
     with Session(engine) as session:
         db_hero = Hero.from_orm(hero)
@@ -70,7 +68,7 @@ def create_hero(hero: HeroCreate):
         return db_hero
 
 
-@app.get("/heroes/", response_model=List[HeroRead])
+@app.get("/heroes", response_model=List[HeroRead])
 def read_heroes():
     with Session(engine) as session:
         return session.exec(select(Hero)).all()
@@ -99,6 +97,21 @@ def update_hero(hero_id: int, hero: HeroUpdate):
         session.refresh(db_hero)
         return db_hero
 
+# @app.put("/heroes/{hero_id}")
+# async def edit_hero(hero_id: int, hero: Hero,  response_model=HeroRead):
+#     with Session(engine) as session:
+#        hero = session.get(Hero, hero_id)
+
+#     if hero is None:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=f"ID {hero_id}: This Hero don't exist"
+#         )
+#     hero.name = hero.name
+
+#     session.add(hero)
+#     session.commit()
+
 
 @app.delete("/heroes/{hero_id}")
 def delete_hero(hero_id: int):
@@ -109,3 +122,5 @@ def delete_hero(hero_id: int):
         session.delete(hero)
         session.commit()
         return {"ok": True}
+
+
