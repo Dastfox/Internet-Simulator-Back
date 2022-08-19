cb2f6d0a07023f55e8d9bfdcbf0aaf025f6a0f7c
Dastfox <88674589+Dastfox@users.noreply.github.com>
2022-08-19 18:20:14 +0200

    Edited to links

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ git clone git@github.com:Dastfox/FastApi.git
 
 # delete previous conda env if any
 conda deactivate
-conda env remove --name FastAPI
+conda env remove --name .env_FastAPI
 
 # create env
-conda create -n .env_FastAPI python=3.8.8 -y
-conda install --force-reinstall -y -q --name FastAPI -c conda-forge --file requirements.txt
+conda create -n env_FastAPI python=3.8.8 -y
+conda install --force-reinstall -y -q --name env_FastAPI -c conda-forge --file requirements.txt
 
 # activate env
-conda activate FastAPI
+conda activate .env_FastAPI
 
 # Launch app
 start http://localhost:8000 && uvicorn src.main:app --reload 
--- src/main.py
+++ src/main.py
@@ -1,36 +1,33 @@
-import string
+from dataclasses import field
+from email.policy import default
 from typing import List, Optional
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException, Query
 
 
+class LinkBase(SQLModel):
+    url: str = Field(index=True)
+    guid: Optional[str] = Field(default=None)
 
 
-class HeroBase(SQLModel):
-    name: str = Field(index=True)
-    image: Optional[str] = Field(default='https://m.media-amazon.com/images/I/51FLAeDVqHL._AC_.jpg')
-
-
-class Hero(HeroBase, table=True):
+class Link(LinkBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    
 
-
-class HeroCreate(HeroBase):
+class LinkCreate(LinkBase):
     pass
 
 
-class HeroRead(HeroBase):
-    id: int
+class LinkRead(LinkBase):
+    id: Optional[int]
+
+class LinkUpdate(SQLModel):
+    url: Optional[str] = None
 
 
-class HeroUpdate(SQLModel):
-    name: Optional[str] = None
-    image: Optional [str] = None
-
-
-sqlite_file_name = "hero.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_file_url = "link.db"
+sqlite_url = f"sqlite:///{sqlite_file_url}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
@@ -40,11 +37,9 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-
-
 app = FastAPI()
 
-origins= ['*']
+origins = ['*']
 
 
 app.add_middleware(
@@ -61,69 +56,37 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/heroes", response_model=HeroRead)
-def create_hero(hero: HeroCreate):
+@app.post("/", response_model=LinkRead)
+def create_link(link: LinkCreate):
     with Session(engine) as session:
-        db_hero = Hero.from_orm(hero)
-        session.add(db_hero)
+        db_link = Link.from_orm(link)
+        session.add(db_link)
         session.commit()
-        session.refresh(db_hero)
-        return db_hero
+        session.refresh(db_link)
+        return db_link
 
 
-@app.get("/heroes", response_model=List[HeroRead])
-def read_heroes():
+@app.get("/", response_model=List[LinkRead])
+def read_links():
     with Session(engine) as session:
-        return session.exec(select(Hero)).all()
+        return session.exec(select(Link)).all()
 
 
-@app.get("/heroes/{hero_id}", response_model=HeroRead)
-def read_hero(hero_id: int):
+@app.get("/details/{link_id}", response_model=LinkRead)
+def read_link(link_id: str):
     with Session(engine) as session:
-        hero = session.get(Hero, hero_id)
-        if not hero:
-            raise HTTPException(status_code=404, detail="Hero not found")
-        return hero
+        link = session.get(Link, link_id)
+        if not link:
+            raise HTTPException(status_code=404, detail="link not found")
+        return link
 
 
-@app.patch("/heroes/{hero_id}", response_model=HeroRead)
-def update_hero(hero_id: int, hero: HeroUpdate):
+@app.delete("/{link_id}")
+def delete_link(link_guid: str):
     with Session(engine) as session:
-        db_hero = session.get(Hero, hero_id)
-        if not db_hero:
-            raise HTTPException(status_code=404, detail="Hero not found")
-        hero_data = hero.dict(exclude_unset=True)
-        for key, value in hero_data.items():
-            setattr(db_hero, key, value)
-        session.add(db_hero)
-        session.commit()
-        session.refresh(db_hero)
-        return db_hero
-
-# @app.put("/heroes/{hero_id}")
-# async def edit_hero(hero_id: int, hero: Hero,  response_model=HeroRead):
-#     with Session(engine) as session:
-#        hero = session.get(Hero, hero_id)
-
-#     if hero is None:
-#         raise HTTPException(
-#             status_code=404,
-#             detail=f"ID {hero_id}: This Hero don't exist"
-#         )
-#     hero.name = hero.name
-
-#     session.add(hero)
-#     session.commit()
-
-
-@app.delete("/heroes/{hero_id}")
-def delete_hero(hero_id: int):
-    with Session(engine) as session:
-        hero = session.get(Hero, hero_id)
-        if not hero:
-            raise HTTPException(status_code=404, detail="Hero not found")
-        session.delete(hero)
+        link = session.get(Link, link_guid)
+        if not link:
+            raise HTTPException(status_code=404, detail="link not found")
+        session.delete(link)
         session.commit()
         return {"ok": True}
-
-
